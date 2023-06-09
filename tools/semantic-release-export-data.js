1a403735e39425b739c80b15adb25d601376e67a
souricevincent <souricevincent@icloud.com>
2023-06-09 11:49:54 +0200

    fix: split set outputs next release data

--- tools/semantic-release-export-data.js
+++ tools/semantic-release-export-data.js
@@ -6,7 +6,9 @@ function verifyConditions() {
 
 function generateNotes(_pluginConfig, {nextRelease}) {
   core.setOutput("next-release-published", "true");
-  core.setOutput("next-release", nextRelease);
+  core.setOutput("next-release-version", nextRelease.version);
+  core.setOutput("next-release-git-tag", nextRelease.gitTag);
+  core.setOutput("next-release-notes", nextRelease.notes);
 }
 
 module.exports = {
