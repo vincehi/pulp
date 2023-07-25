4fd91339517067dcbf1db6e9e47bff246a252c16
souricevincent <souricevincent@icloud.com>
2023-07-25 14:14:05 +0200

    feat: add resize column table

--- src/screens/Main/layouts/ViewFiles/index.tsx
+++ src/screens/Main/layouts/ViewFiles/index.tsx
@@ -7,11 +7,81 @@ import {
   createMemo,
   createResource,
   For,
-  Show,
 } from "solid-js";
 import { type File } from "@prisma/client";
 import { Icon } from "solid-heroicons";
 import { magnifyingGlass } from "solid-heroicons/solid";
+import {
+  createSolidTable,
+  type ColumnDef,
+  getCoreRowModel,
+  flexRender,
+} from "@tanstack/solid-table";
+
+const defaultColumns: Array<ColumnDef<File>> = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: (info) => {
+      const value = info.getValue() as string;
+      return (
+        <div title={value} class="whitespace-nowrap truncate">
+          {value}
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "bpm",
+    header: "BPM",
+    cell: (info) => {
+      const value = info.getValue() as number;
+      return value === 0 ? "-" : Math.round(value);
+    },
+  },
+  {
+    accessorKey: "danceability",
+    header: "Danceability",
+    cell: (info) => {
+      const value = info.getValue() as number;
+      return value === 0 ? "-" : value.toFixed(2);
+    },
+  },
+  {
+    accessorKey: "chordsKey",
+    header: "Chords Key",
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: "chordsScale",
+    header: "Chords Scale",
+    cell: (info) => info.getValue(),
+  },
+  {
+    accessorKey: "show",
+    header: "",
+    cell: (info) => {
+      console.log(info);
+      return (
+        <button
+          onClick={[openInFinder, info.row.original.path]}
+          title="Open in finder"
+        >
+          <Icon
+            path={magnifyingGlass}
+            class="flex-shrink-0 w-4 h-4 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+          />
+        </button>
+      );
+    },
+  },
+];
+
+async function openInFinder(path: string, event: Event): Promise<void> {
+  event.preventDefault();
+  event.stopPropagation();
+  await tauri.invoke("open_in_finder", { path });
+}
 
 const filterStartsWith = (arr: string[]): string[] => {
   return arr.filter((item, index) => {
@@ -32,88 +102,96 @@ const fetchDirectoryFiles: ResourceFetcher<
 };
 const ViewFiles: Component = () => {
   const [store, actions] = useSearch();
-  const computeFilterStartsWith = createMemo(
-    () => [
-      store.search,
-      filterStartsWith(flatMap(store.collapsed, (item) => item.collapsed)),
-    ],
-    [store]
-  );
+  const computeFilterStartsWith = createMemo(() => [
+    store.search,
+    filterStartsWith(flatMap(store.collapsed, (item) => item.collapsed)),
+  ]);
 
   const [files] = createResource<File[], Array<string | string[]>>(
     computeFilterStartsWith,
     fetchDirectoryFiles
   );
 
-  const openInFinder = async (path: string, event: Event): Promise<void> => {
-    event.preventDefault();
-    event.stopPropagation();
-    await tauri.invoke("open_in_finder", { path });
-  };
+  const table = createSolidTable({
+    get data() {
+      return files() ?? [];
+    },
+    columns: defaultColumns,
+    getCoreRowModel: getCoreRowModel(),
+    columnResizeMode: "onChange",
+  });
 
   return (
     <div class="files relative overflow-x-auto shadow-md">
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>BPM</th>
-            <th>Danceability</th>
-            <th>Chords Key</th>
-            <th>Chords Scale</th>
-            <th></th>
-          </tr>
+      <table
+        style={{
+          width: `${table.getTotalSize()}px`,
+        }}
+        class="table table-fixed"
+      >
+        <thead class="sticky top-0 bg-base-100">
+          <For each={table.getHeaderGroups()}>
+            {(headerGroup) => (
+              <tr>
+                <For each={headerGroup.headers}>
+                  {(header) => {
+                    return (
+                      <th
+                        class="whitespace-nowrap overflow-hidden relative select-none"
+                        style={{ width: `${header.getSize()}px` }}
+                      >
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                        <div
+                          onMouseDown={header.getResizeHandler()}
+                          onTouchStart={header.getResizeHandler()}
+                          class="resizer"
+                          classList={{
+                            isResizing: header.column.getIsResizing(),
+                          }}
+                        />
+                      </th>
+                    );
+                  }}
+                </For>
+              </tr>
+            )}
+          </For>
         </thead>
         <tbody>
-          <Show
-            when={!files.loading}
-            fallback={
-              <tr>
-                <td>Loading...</td>
-              </tr>
-            }
-          >
-            <For
-              each={files()}
-              fallback={
-                <tr>
-                  <td>No files found</td>
+          <For each={table.getRowModel().rows}>
+            {(row) => {
+              return (
+                <tr
+                  onClick={(event): void => {
+                    event.currentTarget.focus();
+                  }}
+                  onFocus={[actions.setPathSelected, row.original.path]}
+                  tabIndex={0}
+                  classList={{
+                    "bg-base-200": store.pathSelected === row.original.path,
+                  }}
+                >
+                  <For each={row.getVisibleCells()}>
+                    {(cell) => {
+                      return (
+                        <td class="overflow-hidden">
+                          {flexRender(
+                            cell.column.columnDef.cell,
+                            cell.getContext()
+                          )}
+                        </td>
+                      );
+                    }}
+                  </For>
                 </tr>
-              }
-            >
-              {(file) => {
-                return (
-                  <tr
-                    onClick={(event): void => {
-                      event.currentTarget.focus();
-                    }}
-                    onFocus={[actions.setPathSelected, file.path]}
-                    tabIndex={0}
-                    classList={{
-                      "bg-base-200": store.pathSelected === file.path,
-                    }}
-                  >
-                    <td>{file.name}</td>
-                    <td>{file.bpm}</td>
-                    <td>{file.danceability}</td>
-                    <td>{file.chordsKey}</td>
-                    <td>{file.chordsScale}</td>
-                    <td class="text-right">
-                      <button
-                        onClick={[openInFinder, file.path]}
-                        title="Open in finder"
-                      >
-                        <Icon
-                          path={magnifyingGlass}
-                          class="flex-shrink-0 w-4 h-4 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                        />
-                      </button>
-                    </td>
-                  </tr>
-                );
-              }}
-            </For>
-          </Show>
+              );
+            }}
+          </For>
         </tbody>
       </table>
     </div>
