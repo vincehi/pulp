58c73797dc8d86095ecbb015d19ef8ca87d1dbb9
VinceHi <souricevincent@icloud.com>
2024-06-28 12:06:17 +0200

    fix: unselect on blur tab input renaming

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -32,17 +32,23 @@ const Tabs: FlowComponent<Props, Component<any>> = (props) => {
                 onClick={() => props.setActive(index())}
                 class="tab flex-shrink-0"
                 classList={{
-                  "tab-active": item.active,
+                  "tab-active": item.active
                 }}
               >
-                <label class="input-sizer" data-value={item.name}>
+                <label classList={{
+                  "cursor-pointer": !getRenamingMode()
+                }} class="input-sizer" data-value={item.name}>
                   <input
-                    classList={{ "pointer-events-none": !getRenamingMode() }}
+                    classList={{
+                      "pointer-events-none": !getRenamingMode(),
+                    }}
                     ref={setTarget}
                     type="text"
                     value={item.name}
                     disabled={!getRenamingMode()}
-                    onBlur={() => setRenamingMode(false)}
+                    onBlur={(event) => {
+                      window.getSelection()?.removeAllRanges(); // TODO: unselectinput
+                      setRenamingMode(false)}}
                     onInput={(event) => {
                       tabsStore.rename(index(), event.target.value);
                     }}
