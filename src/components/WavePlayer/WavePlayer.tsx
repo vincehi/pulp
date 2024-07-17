627b10e292b98a105b7cdb7aea0ef1900d6b36ca
souricevincent <souricevincent@icloud.com>
2024-07-17 18:11:41 +0200

    fix: add wavesurfer-solidjs package

--- src/components/WavePlayer/WavePlayer.tsx
+++ src/components/WavePlayer/WavePlayer.tsx
@@ -3,76 +3,56 @@ import { openInFinder } from "@/services/filesServices";
 import { convertFileSrc } from "@tauri-apps/api/tauri";
 import { Icon } from "solid-heroicons";
 import { magnifyingGlass, pause, play } from "solid-heroicons/solid";
-import { createSignal, type Component } from "solid-js";
-import WaveSurfer from "wavesurfer.js";
-import { WavesurferPlayer } from "./hooksWavesurfer";
-// import styles from "./App.module.css";
+import { Match, Switch, createEffect, on, type Component } from "solid-js";
+import { useLocalStorage } from "solidjs-use";
+import { createWavesurfer } from "wavesurfer-solidjs";
 
 // https://dolby.io/blog/how-to-visualize-and-annotate-your-audio-with-wavesurfer-js-and-konva-in-solidjs/
-const WavePlayer: Component = () => {
+const Waveplayer: Component = () => {
   const [store] = useSearch();
-  let container!: HTMLDivElement, // Auto-referenced by the returning JSX
-    wavesurfer: WaveSurfer;
+  const [autoPlay, setAutoplay] = useLocalStorage("autoPlay", false);
 
-  const [isPlaying, setIsPlaying] = createSignal(false);
+  let container!: HTMLDivElement;
 
-  // const [autoPlay, setAutoplay] = useLocalStorage("autoPlay", false);
+  const { wavesurfer, isPlaying, isReady } = createWavesurfer({
+    getContainer: () => container,
+    get url() {
+      return convertFileSrc(store.pathSelected);
+    },
+    waveColor: "#DC2B20",
+    progressColor: "grey",
+    height: 100,
+    backend: "WebAudio",
+  });
 
-  // onMount(() => {
-  //   wavesurfer = WaveSurfer.create({
-  //     container,
-  //     waveColor: "violet",
-  //     progressColor: "purple",
-  //     interact: false,
-  //   });
-  //   wavesurfer.on("ready", function () {
-  //     console.log("One init");
-  //     if (autoPlay()) {
-  //       void wavesurfer.play();
-  //     }
-  //   });
-  //   wavesurfer.on("play", () => {
-  //     setIsPlaying(true);
-  //   });
-  //   wavesurfer.on("pause", () => setIsPlaying(false));
-  //   wavesurfer.on("finish", () => setIsPlaying(false));
-  // });
+  createEffect(
+    on(
+      isReady,
+      (v) => {
+        if (v && autoPlay()) {
+          void wavesurfer()?.play();
+        }
+      },
+      { defer: true }
+    )
+  );
 
-  // createEffect(() => {
-  //   const file = store.pathSelected;
-  //   if (file !== "") {
-  //     void wavesurfer.load(convertFileSrc(file));
-  //   }
-  // });
-
-  // const handlePlayPause = (event: Event): void => {
-  //   event.preventDefault();
-  //   void wavesurfer.playPause();
-  // };
-
-  const [ws, setWavesurfer] = createSignal(null);
-  // const [isPlaying, setIsPlaying] = createSignal(false);
-
-  const onReady = (ws) => {
-    setWavesurfer(ws);
-    setIsPlaying(false);
-  };
-
-  const onPlayPause = () => {
-    ws() && ws().playPause();
+  const handlePlayPause = (event: Event): void => {
+    event.preventDefault();
+    void wavesurfer()?.playPause();
   };
 
   return (
     <div class="player p-4">
       <div class="flex items-center mb-4 gap-4">
         <div class="flex items-center">
-          {/* <input
+          <input
             id="default-checkbox"
             type="checkbox"
             checked={autoPlay()}
             onChange={() => setAutoplay((prev) => prev === false)}
             class="checkbox checkbox-sm"
-          /> */}
+          />
           <label
             for="default-checkbox"
             class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -92,28 +72,17 @@ const WavePlayer: Component = () => {
           />
         </button>
       </div>
-      {/* <div ref={container} /> */}
-
-      <WavesurferPlayer
-        height={100}
-        waveColor="violet"
-        url={convertFileSrc(store.pathSelected)}
-        onReady={onReady}
-        onPlay={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
-      />
-
-      <button onClick={onPlayPause}>{isPlaying() ? "Pause" : "Play"}</button>
+      <div ref={(el) => (container = el)} />
 
       <label class="swap">
-        {/* <Switch>
+        <Switch>
           <Match when={isPlaying()}>
             <input type="checkbox" checked={true} onClick={handlePlayPause} />
           </Match>
           <Match when={!isPlaying()}>
             <input type="checkbox" checked={false} onClick={handlePlayPause} />
           </Match>
-        </Switch> */}
+        </Switch>
         <Icon
           path={play}
           class="swap-off flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
@@ -126,4 +95,4 @@ const WavePlayer: Component = () => {
     </div>
   );
 };
-export default WavePlayer;
+export default Waveplayer;
